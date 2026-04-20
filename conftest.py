10e8e85456928bed3492304262fbbcc85d768440
gyouki1 <984308746@qq.com>
2026-04-21 02:13:15 +0800

    add allure report

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 import json
 import requests
 
-@pytest.fixture
+@pytest.fixture         #夹具：读取数据，发请求，准备环境
 def test_data():
     with open("test_data.json", "r", encoding="utf-8") as f:
         return json.load(f)
@@ -10,3 +10,5 @@ def test_data():
 @pytest.fixture
 def base_url(test_data):
     return test_data["base_url"]
+
+
